aa3fe1f87dafac6114e309ce6b2973d1e4149c13
Mohamed Houssein Douici <douici1@gmail.com>
2024-12-01 15:15:56 +0100

    chore: add prisma to read from db

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,22 +1,5 @@
 import type { CartItem, Order, Product } from "@/types";
 
-export const products: Product[] = [
-	{
-		id: "1",
-		name: "Smartphone",
-		description: "Latest model smartphone",
-		price: 999.99,
-		stock: 50,
-	},
-	{
-		id: "2",
-		name: "Laptop",
-		description: "High-performance laptop",
-		price: 1499.99,
-		stock: 30,
-	},
-];
-
 export const carts: Map<string, CartItem[]> = new Map();
 export const orders: Order[] = [];
 
